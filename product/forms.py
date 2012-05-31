c7299c463cc09cb390904e19561fc5d4146a9dc1
Sam Liu <genxstylez@gmail.com>
2012-06-01 01:23:52 +0800

    fixed product option

--- product/forms.py
+++ product/forms.py
@@ -1,18 +1,9 @@
 from django import forms
 from product.models import *
 
-TYPE_CHOICES = [(0, '----')]
-TYPE_CHOICES += [(type.id, type.name) for type in ProductVariationType.objects.all()]
+#TYPE_CHOICES = [(0, '----')]
+#TYPE_CHOICES += [(type.id, type.name) for type in ProductVariationType.objects.all()]
 
 class ProductForm(forms.ModelForm):
-    variation_type = forms.ChoiceField(required=True, choices=TYPE_CHOICES)
-    variations = forms.ChoiceField(required=False)
-
-    def clean_variations(self):
-        if self.cleaned_data['variation_type'] == 0 and not self.cleaned_data['variations']:
-            raise forms.ValidationError(_('Variations required'))
-
-        return self.cleaned_data['variations']
-         
     class Meta:
         model = Product
--- product/models.py
+++ product/models.py
@@ -23,35 +23,38 @@ class Product(models.Model):
     category = models.ForeignKey(Category, verbose_name=_('Category'), related_name='products')
     brand = models.ForeignKey(Brand, verbose_name=_('Brand'), related_name='products')
     price = models.PositiveIntegerField(_('Price'), default=0)
+    has_options = models.BooleanField(_('Has options'), default=False)
+    
+    def __unicode__(self):
+        return self.name
+
+class OptionProduct(models.Model):
+    description = models.TextField(_('Description'))
+    product = models.ForeignKey(Product, verbose_name=_('Main product'), related_name='option_products')
+    stock = models.IntegerField(_('Stock'), default=0)
+    category = models.ForeignKey(Category, verbose_name=_('Category'), related_name='option_products')
+    brand = models.ForeignKey(Brand, verbose_name=_('Brand'), related_name='option_products')
+    price = models.PositiveIntegerField(_('Price'), default=0)
      
     def __unicode__(self):
-        return self.name
 
-class ProductImage(models.Model):
+        if self.option_set.count() > 0:
+            name = '' 
+            for option in self.option_set.all():
+                name += ' - ' + option.name
+            return self.product.name + name
 
-    def product_image_path(self, filename):
-        return 'product_images/%s/%s' % (self.product.brand, self.product)
-
-    product = models.ForeignKey(Product, related_name='images')
-    image = models.ImageField(_('Image'), upload_to=product_image_path, blank=True)
-
-class ProductVariation(models.Model):
-    name = models.CharField(_('Variation'), max_length=100)
-    stock = models.IntegerField(_('Stock'), default=0)
-    price = models.PositiveIntegerField(_('Price'), default=0)
-    product = models.ForeignKey(Product, related_name='variations')
-
-    def __unicode__(self):
-        return self.name
+        return self.product.name
 
     def save(self):
         total_stock = 0
-        for variation in self.product.variations.all():
-            total_stock += variation.stock
-        self.product.stock = total_stock
+        for option_product in self.product.option_products.all():
+            total_stock += option_product.stock
+        self.product.stock = total_stock + self.stock
+        self.product.has_options = True
         self.product.save()
 
-        super(ProductVariation, self).save()
+        super(OptionProduct, self).save()
 
     def get_price(self):
         if self.price > 0:
@@ -59,12 +62,27 @@ class ProductVariation(models.Model):
         else:
             return self.product.price
 
-class ProductVariationType(models.Model):
-    name = models.CharField(_('Variation'), max_length=100)
-    product = models.ManyToManyField(Product, blank=True)
-    variations = models.ManyToManyField(ProductVariation, verbose_name=_('Variations'), blank=True)
-    
+
+class ProductImage(models.Model):
+
+    def product_image_path(self, filename):
+        return 'product_images/%s/%s' % (self.product.brand, self.product)
+
+    product = models.ForeignKey(Product, related_name='images', null=True, blank=True)
+    option_product = models.ForeignKey(OptionProduct, related_name='images', blank=True, null=True)
+    image = models.ImageField(_('Image'), upload_to=product_image_path, blank=True)
+
+class OptionGroup(models.Model):
+    name = models.CharField(_('Option Group'), max_length=100)
+    products = models.ManyToManyField(OptionProduct)
+
     def __unicode__(self):
         return self.name
 
-
+class Option(models.Model):
+    name = models.CharField(_('Option'), max_length=100)
+    option_group = models.ForeignKey(OptionGroup, verbose_name=_('Option Group'), related_name='options')
+    products = models.ManyToManyField(OptionProduct)
+    
+    def __unicode__(self):
+        return self.name
