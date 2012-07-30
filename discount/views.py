1a9255462a9d41b132573b5aeb7776c6649b9fd4
genxstylez <genxstylez@gmail.com>
2012-07-30 06:49:45 -0700

    discount to be done

--- discount/views.py
+++ discount/views.py
@@ -3,5 +3,11 @@ from discount.forms import DiscountForm
 
 def index(request):
     form = DiscountForm(request.POST or None)
+    if form.is_valid():
+        discount = form.save(commit=False)
+        for brand in discount.valid_brands.all():
+            for product in brand.products.all():
+                discount.valid_products = product.id
+        discount.save_m2m()
 
     return render(request, 'discount/index.html', {'form': form})
