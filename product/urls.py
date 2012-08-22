1fff0ad0dbf83a8606422bd0d25e31a6924a237c
Sam Liu <genxstylez@gmail.com>
2012-08-23 02:56:22 +0800

    fix minor bug with url

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('product.views.site',
-    url(r'^(?P<product_slug>\w+)/$', 'product_view', name='product-view'),
+    url(r'^(?P<brand>\w+)/(?P<product_slug>\w+)/$', 'product_view', name='product-view'),
 )
