e7a867795a14ed657428f529dba18dc3c1377d54
genxstylez <genxstylez@gmail.com>
2012-08-22 14:32:24 +0000

    prefetch related added

--- order/settings.py
+++ order/settings.py
@@ -1,13 +1,13 @@
 from django.utils.translation import ugettext_lazy as _
 
-ORDER_STATUS_CHOICES = (
+ORDER_STATUS_CHOICES = [
     (0, _('Pending for payment')),
     (1, _('Paid / Dispatching')),
     (2, _('Deferred / Expired')),
     (3, _('Completed'))
-)
+]
 
-PAYMENT_METHOD_CHOICES = (
+PAYMENT_METHOD_CHOICES = [
     (0, _('PayPal')),
     (1, _('COD')),
-)
+]
--- product/views/site.py
+++ product/views/site.py
@@ -1,14 +1,18 @@
 from django.utils.translation import ugettext as _
-from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.shortcuts import render
 from product.models import Product
 
 def product_view(request, brand, product_slug):
-    focus_product = get_object_or_404(Product, slug=product_slug)
+    try:
+        focus_product = Product.objects.prefetch_related('brand', 'option_group').get(brand__name=brand, slug=product_slug)
+    except Product.DoesNotExist:
+        raise Http404
 
     if request.is_ajax():
         return render(request, 'product/site/product-ajax.html', {'focus_product': focus_product})
-
-    products = Product.objects.all()
+    
+    products = Product.objects.all().prefetch_related('brand', 'option_group')
     box_class = ['a11', 'a12', 'a21', 'a22']
 
     return render(request, 'product/site/product.html', {'products': products, 'focus_product': focus_product, 'box_class': box_class, 'popup': True})
