ebc64b09a34362737af665a92f23e263c9229331
genxstylez <genxstylez@gmail.com>
2012-09-14 08:01:42 +0000

    mysql instead of sqlite

--- empor/settings.py
+++ empor/settings.py
@@ -14,10 +14,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', #or 'oracle'.
-        'NAME': os.path.join(DIRNAME, 'simple.db'), # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', #or 'oracle'.
+        'NAME': 'empor', # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '123',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
