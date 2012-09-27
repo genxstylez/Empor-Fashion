f449262d7f1ade7ee67ccb7a155c4b75115d465f
genxstylez <genxstylez@gmail.com>
2012-09-27 08:15:19 +0000

    try to fix thumb

--- empor/thumbs.py
+++ empor/thumbs.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import hashlib
-from cStringIO import StringIO
 from django.core.files.base import ContentFile
+import cStringIO
 
 def generate_crop(file, ext, x1, y1, x2, y2):
     image = Image.open(file)
@@ -10,13 +10,13 @@ def generate_crop(file, ext, x1, y1, x2, y2):
     if image.mode not in ("L", "RGB"):
         image = image.convert("RGB")
 
-    image = image.crop((x1,y1,x2,y2))
+    cropped = image.crop((x1,y1,x2,y2))
 
     if image.size != (40,40):
-        image = thumb_resize(image, ext, 40, 40)
+        cropped = thumb_resize(image, ext, 40, 40)
 
     else:
-        imagefile = StringIO()
+        imagefile = cStringIO.StringIO()
         filename = hashlib.md5(imagefile.getvalue()).hexdigest()+ '.' + ext
         if ext.upper() == 'JPG':
                 ext = 'JPEG'
@@ -24,7 +24,7 @@ def generate_crop(file, ext, x1, y1, x2, y2):
         content = ContentFile(imagefile.getvalue())
         image = (filename, content)
 
-    return image
+    return cropped
 
 def thumb_resize(file, ext, dimension1, dimension2=None):
     if isinstance(file, Image.Image):
@@ -38,11 +38,11 @@ def thumb_resize(file, ext, dimension1, dimension2=None):
         dimension2 = sizes[1]/ratio 
     image = image.resize((dimension1, dimension2), Image.ANTIALIAS)
 
-    imagefile = StringIO()
-    filename = hashlib.md5(imagefile.getvalue()).hexdigest()+ '.' + ext
+    io = cStringIO.StringIO()
     if ext.upper() == 'JPG':
         ext = 'JPEG'
-    image.save(imagefile, ext, quality=95)
-    content = ContentFile(imagefile.getvalue())
+    image.save(io, ext, quality=95)
+    content = ContentFile(io.getvalue())
+    filename = hashlib.md5(io.getvalue()).hexdigest()+ '.' + ext
 
     return (filename, content)
