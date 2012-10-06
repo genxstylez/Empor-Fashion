6205c683dde0faf6cd071b38665360361f4a0587
Sam <genxstylez@gmail.com>
2012-10-06 02:24:19 -0700

    fix js

--- empor/static/js/app.js
+++ empor/static/js/app.js
@@ -143,8 +143,8 @@ function init(){
             var height = parseInt(330 / img.width() * img.height())
             that.hide();
             $(image).animate({
-                'left': $(window).width() / 2 - 400,
-                'top': $(window).height() * 0.08 + 20,
+                'left': $(window).width() / 2 - 399,
+                'top': $(window).height() * 0.08 + 22,
                 'height': height,
                 'width': '330px',
             }, 400,  function() {
