8f3c0a35aedfc5c5af6288ef68776422bf436b13
Sam <genxstylez@gmail.com>
2012-10-06 02:12:25 -0700

    try to fix js

--- empor/static/js/app.js
+++ empor/static/js/app.js
@@ -144,7 +144,7 @@ function init(){
             that.hide();
             $(image).animate({
                 'left': $(window).width() / 2 - 345,
-                'top': $(window).height() * 0.08 + 15,
+                'top': $(window).height() * 0.08 + 25,
                 'height': height,
                 'width': '330px',
             }, 400,  function() {
