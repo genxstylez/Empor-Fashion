e2859054dda8332b24050c16840a881f973a4c19
Sam <genxstylez@gmail.com>
2012-10-06 02:11:25 -0700

    done jst pu

--- empor/static/js/app.js
+++ empor/static/js/app.js
@@ -144,7 +144,7 @@ function init(){
             that.hide();
             $(image).animate({
                 'left': $(window).width() / 2 - 345,
-                'top': $(window).height() * 0.08 + 20,
+                'top': $(window).height() * 0.08 + 15,
                 'height': height,
                 'width': '330px',
             }, 400,  function() {
