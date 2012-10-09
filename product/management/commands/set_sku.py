9826092c2fc6b8590418ca6e896f8706c0a6362d
Sam <genxstylez@gmail.com>
2012-10-09 14:05:29 -0700

    set sku refined

--- product/management/commands/set_sku.py
+++ product/management/commands/set_sku.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
-from product.models import Product
+from product.models import Product, Collection
 
 class Command(BaseCommand):
     def handle(self, **options):
-        products = Product.objects.filter(parent=None)
+        collection = Collection.objects.all()
+        products = Product.objects.filter(parent=None, collection=collection)
         for i, product in enumerate(products, start=1):
             value = i
             product.sku = product.brand.name[:3].upper() + '%04d%04d' % (product.collection.id , value)
