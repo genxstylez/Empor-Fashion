b8acbb27af8441b4ae9c9b3709b20eb377681165
Sam <genxstylez@gmail.com>
2012-10-14 12:04:08 -0700

    fixed cart bug

--- empor/settings.py
+++ empor/settings.py
@@ -105,6 +105,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -116,6 +117,7 @@ ROOT_URLCONF = 'empor.urls'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
+    'django.contrib.messages.context_processors.messages',
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
--- empor/urls.py
+++ empor/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from member.forms import RegisterForm, ProfileForm
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,8 +14,4 @@ urlpatterns = patterns('',
     url(r'^discount/', include('discount.urls')),
     url(r'^order/', include('order.urls')),
     url(r'^service/', include('service.urls')),
-    url(r'^accounts/password/reset/$', auth_views.password_reset, 
-        {'template_name': 'userena/password_reset_form.html',
-        'email_template_name': 'userena/emails/password_reset_message.txt'},
-        name="member-password-reset"),
 )
