6caf7bfd0c28b0e88a81dfbe06ea6c7501298119
Sam Liu <sam@streetvoice.com>
2012-10-25 12:13:12 +0000

    sitemap add more url

--- empor/sitemap.py
+++ empor/sitemap.py
@@ -3,14 +3,16 @@ from django.contrib.sitemaps import GenericSitemap, Sitemap
 from product.models import Product
 
 class ViewSitemap(Sitemap):
+    priority = 1
+    changefreq = 'monthly'
     def items(self):
-        return ['index']
-
+        return ['index', 'service-faq', 'service-center']
+    
     def location(self, item):
         return reverse(item)
-
+    
 product = {'queryset': Product.on_site.all(), 'date_field': 'last_modified'}
 sitemaps = {
     'views': ViewSitemap,
-    'products': GenericSitemap(product, priority=0.9, changefreq='daily'),
+    'products': GenericSitemap(product, priority=0.8, changefreq='daily'),
 }
