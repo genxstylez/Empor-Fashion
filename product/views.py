bcdc2c5ca0fb78e2a0a0e893a0a386156a5cf168
Sam Liu <genxstylez@gmail.com>
2012-11-19 14:32:50 -0200

    fix views and js

--- product/views.py
+++ product/views.py
@@ -15,7 +15,7 @@ def brand(request, brand_slug):
     brand = Brand.objects.get(slug=brand_slug) 
     products = Product.on_site.filter(brand=brand)
     box_class = ['a11', 'a12', 'a21', 'a22']
-    return render(request, 'product/brand.html', {'brand': brand, 'products': products, 'box_class': box_class})
+    return render(request, 'product/brand.html', {'brand': brand, 'products': products, 'box_class': box_class, 'popup': True})
 
 def brand_products(request, brand_slug, gender_type=None, category=None):
     brand = Brand.objects.get(slug=brand_slug)
