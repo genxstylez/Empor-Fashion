24b39991687a7219cb2821792838bf67e24484f2
Sam Liu <sam@empor.com.tw>
2012-12-24 15:06:33 +0000

    fix dojo editor

--- empor/static/js/dojo/editor.js
+++ empor/static/js/dojo/editor.js
@@ -1,14 +1,14 @@
 dojo.require("dijit.Editor");
 
 // extra plugins
-dojo.require("dijit._editor.plugins.FontChoice");
+//dojo.require("dijit._editor.plugins.FontChoice");
 dojo.require("dojox.editor.plugins.TextColor");
 dojo.require("dojox.editor.plugins.Blockquote");
 dojo.require("dijit._editor.plugins.LinkDialog");
 dojo.require("dojox.editor.plugins.InsertAnchor");
 dojo.require("dojox.editor.plugins.FindReplace");
 dojo.require("dojox.editor.plugins.ShowBlockNodes");
-dojo.require("dojox.editor.plugins.PasteFromWord");
+//dojo.require("dojox.editor.plugins.PasteFromWord");
 dojo.require("dijit._editor.plugins.ViewSource");
 dojo.require("dijit._editor.plugins.FullScreen");
 dojo.require("dojox.editor.plugins.InsertEntity");
@@ -32,11 +32,11 @@ dojo.ready(function(){
         "cut", "copy", "paste", "|",
         "bold", "italic", "underline", "strikethrough", "|",
         "insertOrderedList", "insertUnorderedList", "indent", "outdent", "||",
-        "formatBlock", "fontName", "fontSize", "||",
+        //"formatBlock", "fontName", "fontSize", "||",
         "findreplace", "insertEntity", "blockquote", "|",
-        "createLink", "insertImage", "insertanchor", "|",
-        "foreColor", "hiliteColor", "|",
-        "showblocknodes", "pastefromword",
+       // "createLink", "insertImage", "insertanchor", "|",
+         "foreColor", "hiliteColor", "|",
+//        "showblocknodes", "pastefromword",
         // headless plugins
         "normalizeindentoutdent", "prettyprint",
         "autourllink", "dijit._editor.plugins.EnterKeyHandling"
